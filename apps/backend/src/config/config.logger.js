6821d199dc4c2cd11f63e716e6dd5c7a13a3d724
Atharv Dixit <atharvdixit06@gmail.com>
2026-05-06 22:26:08 +0530

    fixed deployment issues

--- apps/backend/src/config/config.logger.js
+++ apps/backend/src/config/config.logger.js
@@ -1,10 +1,13 @@
 import pino from "pino";
+import { config } from "./config.env.js";
 
 const logger = pino({
   level: "info",
-  transport: {
-    target: "pino-pretty"
-  }
+  ...(config.NODE_ENV !== "production" && {
+    transport: {
+      target: "pino-pretty"
+    }
+  })
 });
 
 export default logger;
